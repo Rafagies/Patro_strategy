67adc0f2ed78263a40b78f5c95b9deef9066fd39
Rafagimenez <Rafagimenez@RMini-PC>
2014-06-03 09:16:26 +0200

    Acabe d'introduir el primer projecte al directory del repositori.

---
+++ patro_strategy_equip/src/patro_strategy_equip/Patro_strategy_equip.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patro_strategy_equip;
+
+/**
+ *
+ * @author Rafagimenez
+ */
+public class Patro_strategy_equip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Aquest es el primer fitxer del projecte que creen
+    }
+}
