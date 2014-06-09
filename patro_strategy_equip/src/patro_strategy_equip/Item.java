9b475908c509c30ea9a4d43609aa0e616d1dcb8b
Rafagimenez <Rafagimenez@RMini-PC>
2014-06-09 11:34:44 +0200

    Totes les classes

--- patro_strategy_equip/src/patro_strategy_equip/Item.java
+++ patro_strategy_equip/src/patro_strategy_equip/Item.java
@@ -1,17 +1,24 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Exercici de Javadoc
  */
 package patro_strategy_equip;
 
-/**
+/* 
+ * Cal que fiques nom d'autor el teu, versio la teua edat, 
+ * 
+ * Crea una branca del projecte amb el teu nom i el tornes a pujar al respositori amb el nom de la teua branca
  *
- * @author Rafagimenez
  */
 public class Item {
     private String upcCode;
     private int price;
-     
+    
+             
+   /**
+    * per a cada métode descriu els paràmetres per a generar el javadoc
+    */ 
+    
+    
     public Item(String upc, int cost){
         this.upcCode=upc;
         this.price=cost;
