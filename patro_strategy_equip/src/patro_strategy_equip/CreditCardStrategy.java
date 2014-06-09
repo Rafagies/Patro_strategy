987591bcb54369ec78f2ecda6cbff783ad198f6a
Rafagimenez <Rafagimenez@RMini-PC>
2014-06-09 11:37:31 +0200

    Exercici Javadoc

--- patro_strategy_equip/src/patro_strategy_equip/CreditCardStrategy.java
+++ patro_strategy_equip/src/patro_strategy_equip/CreditCardStrategy.java
@@ -5,7 +5,6 @@ package patro_strategy_equip;
 
 /* 
  * Cal que fiques nom d'autor el teu, versio la teua edat, 
- * 
  * Crea una branca del projecte amb el teu nom i el tornes a pujar al respositori amb el nom de la teua branca
  *
  */
