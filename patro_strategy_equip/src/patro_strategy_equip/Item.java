fb939926500436cbbda03d3d80b3bcc2c8efa510
alumne <alumne@professor-Latitude-E5400>
2014-06-09 12:49:20 +0200

    He posat el nom i la versió

--- patro_strategy_equip/src/patro_strategy_equip/Item.java
+++ patro_strategy_equip/src/patro_strategy_equip/Item.java
@@ -3,7 +3,8 @@
  */
 package patro_strategy_equip;
 
-/* 
+/*  @author Raul Mulet
+ * @versio 34
  * Cal que fiques nom d'autor el teu, versio la teua edat, 
  * Crea una branca del projecte amb el teu nom i el tornes a pujar al respositori amb el nom de la teua branca
  *
