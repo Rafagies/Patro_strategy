553c50460a45485b49f4a718d21e347f52f4bb2e
Rafagimenez <Rafagimenez@RMini-PC>
2014-06-09 11:34:38 +0200

    Totes les classes

--- patro_strategy_equip/src/patro_strategy_equip/CreditCardStrategy.java
+++ patro_strategy_equip/src/patro_strategy_equip/CreditCardStrategy.java
@@ -1,58 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Exercici de Javadoc
  */
 package patro_strategy_equip;
 
-/**
- * Graphics is the abstract base class for all graphics contexts
- * which allow an application to draw onto components realized on
- * various devices or onto off-screen images.
- * A Graphics object encapsulates the state information needed
- * for the various rendering operations that Java supports.  This
- * state information includes:
- * <ul>
- * <li>The Component to draw on
- * <li>A translation origin for rendering and clipping coordinates
- * <li>The current clip
- * <li>The current color
- * <li>The current font
- * <li>The current logical pixel operation function (XOR or Paint)
- * <li>The current XOR alternation color
- *     (see <a href="#setXORMode">setXORMode</a>)
- * </ul>
- * <p>
- * Coordinates are infinitely thin and lie between the pixels of the
- * output device.
- * Operations which draw the outline of a figure operate by traversing
- * along the infinitely thin path with a pixel-sized pen that hangs
- * down and to the right of the anchor point on the path.
- * Operations which fill a figure operate by filling the interior
- * of the infinitely thin path.
- * Operations which render horizontal text render the ascending
- * portion of the characters entirely above the baseline coordinate.
- * <p>
- * Some important points to consider are that drawing a figure that
- * covers a given rectangle will occupy one extra row of pixels on
- * the right and bottom edges compared to filling a figure that is
- * bounded by that same rectangle.
- * Also, drawing a horizontal line along the same y coordinate as
- * the baseline of a line of text will draw the line entirely below
- * the text except for any descenders.
- * Both of these properties are due to the pen hanging down and to
- * the right from the path that it traverses.
- * <p> <HR>
- * All coordinates which appear as arguments to the methods of this
- * Graphics object are considered relative to the translation origin
- * of this Graphics object prior to the invocation of the method.
- * All rendering operations modify only pixels which lie within the
- * area bounded by both the current clip of the graphics context
- * and the extents of the Component used to create the Graphics object.
+/* 
+ * Cal que fiques nom d'autor el teu, versio la teua edat, 
  * 
- * @author      Sami Shaio
- * @author      Arthur van Hoff
- * @version     %I%, %G%
- * @since       1.0
+ * Crea una branca del projecte amb el teu nom i el tornes a pujar al respositori amb el nom de la teua branca
+ *
  */
 
 
@@ -63,11 +18,13 @@ public class CreditCardStrategy implements PaymentStrategy {
     private String cvv;
     private String dateOfExpiry;
      
+         
+   /**
+    * per a cada métode descriu els paràmetres per a generar el javadoc
+    */ 
+    
     public CreditCardStrategy(String nm, String ccNum, String cvv, String expiryDate){
-        /** * Calcula el factorial de n. * n! = n * (n-1) * (n-2) * (n-3) * ... 
-         * 1 
-         * @param n és el número al que es calcularà el factorial. 
-         * @return n! és el resultat del factorial de n */ 
+   
         this.name=nm;
         this.cardNumber=ccNum;
         this.cvv=cvv;
@@ -75,33 +32,6 @@ public class CreditCardStrategy implements PaymentStrategy {
     }
     @Override
     public void pay(int amount) {
-    /** 
-     * Draws as much of the specified image as is currently available
-     * with its northwest corner at the specified coordinate (x, y).
-     * This method will return immediately in all cases, even if the
-     * entire image has not yet been scaled, dithered and converted
-     * for the current output device.
-     * <p>
-     * If the current output representation is not yet complete then
-     * the method will return false and the indicated 
-     * {@link ImageObserver} object will be notified as the
-     * conversion process progresses.
-     *
-     * @param img       the image to be drawn
-     * @param x         the x-coordinate of the northwest corner
-     *                  of the destination rectangle in pixels
-     * @param y         the y-coordinate of the northwest corner
-     *                  of the destination rectangle in pixels
-     * @param observer  the image observer to be notified as more
-     *                  of the image is converted.  May be 
-     *                  <code>null</code>
-     * @return          <code>true</code> if the image is completely 
-     *                  loaded and was painted successfully; 
-     *                  <code>false</code> otherwise.
-     * @see             Image
-     * @see             ImageObserver
-     * @since           1.0
-     */
         System.out.println(amount +" paid with credit/debit card");
     }
 }
